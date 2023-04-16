c713cdf86cbbaab9cc4118094b702be3312f6307
PolamW <pwong140@my.centennialcollege.ca>
2023-04-16 13:51:44 -0400

    add scroll view

--- PhoneOrderApp/ConfirmationViewController.swift
+++ PhoneOrderApp/ConfirmationViewController.swift
@@ -36,7 +36,6 @@ class ConfirmationViewController: UIViewController {
         self.order = order
     }
     
-    
     private var customerRepository: CustomerRepository? = nil
     private var phoneOrderRepository: PhoneOrderRepository? = nil
     
@@ -51,9 +50,6 @@ class ConfirmationViewController: UIViewController {
         errorMsgLabel.isHidden = true
         customerRepository = CustomerRepository(delegate: self)
         phoneOrderRepository = PhoneOrderRepository(delegate: self)
-        
-        creditCardNumTextField.allTargets.suffix(16)
-        
         
         loadExistingCustomerInfo()
     }
